40cc171abdb242ac9b8864c1a12e3e88f3d758c4
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2024-03-30 00:18:47 -0600

    r

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Image from 'next/image'
+import image from "../../public/sait.png"
 import Link from "next/link"
 
 export default function Page() {
     return <>
 
-        <p className="text-2xl">SAIT</p>
-        <div className="relative border-2 w-3/5 mx-auto mt-16 border-white py-14 text-center">
+        <Image src={image} alt="sait logo" className="w-min cursor-pointer" width={40} height={40} />        <div className="relative border-2 w-3/5 mx-auto mt-16 border-white py-14 text-center">
             <div className="w-fit mx-auto">
                 <p className="text-left mb-2">Student ID</p>
                 <input placeholder="Please enter your student ID" className="p-2 outline outline-2 outline-white placeholder-black w-72" type="number" />
