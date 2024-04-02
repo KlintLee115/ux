6b4d13ef46c17b6a1a6bc0e5b9f76318c0cc4542
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2024-04-02 08:23:59 -0600

    t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,24 @@ export default function Page() {
     const [type, setType] = useState<HTMLInputTypeAttribute>('password');
     const [icon, setIcon] = useState(eyeOff);
 
+    const login = () => {
+        if (id === 1234 && pwd === "password") {
+            router.push('/appContent/academics');
+        }
+        else {
+            Notifications.NotificationQueue.push({
+                action: ActionType.ERROR,
+            })
+            Notifications.updateNotificationQueue && Notifications.updateNotificationQueue();
+        }
+    };
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            login();
+        }
+    };
+
     const handleToggle = () => {
         if (type === 'password') {
             setIcon(eye);
@@ -33,7 +51,10 @@ export default function Page() {
         <Image src={image} alt="sait logo" className="w-min cursor-pointer" width={40} height={40} />
         <div className="relative border-2 w-3/5 mx-auto mt-16 border-white py-14 text-center">
 
-            <input placeholder="Please enter your student ID" onChange={event => setId(event.target.valueAsNumber)} className="p-2 outline outline-2 outline-white placeholder-black w-72" type="number" />
+            <input placeholder="Please enter your student ID"
+                onKeyDown={handleKeyPress}
+                onChange={event => setId(event.target.valueAsNumber)}
+                className="p-2 outline outline-2 outline-white placeholder-black w-72" type="number" />
 
             <div className="w-72 mx-auto mt-16  bg-white p-2">
                 <input
@@ -42,25 +63,14 @@ export default function Page() {
                     className='placeholder-black outline-none'
                     placeholder="Password"
                     value={pwd}
+                    onKeyDown={handleKeyPress}
                     onChange={(e) => setPwd(e.target.value)}
                     autoComplete="current-password"
                 />
                 <Icon className="text-black" onClick={handleToggle} icon={icon} size={25} />
             </div>
 
-            <button className="block w-fit mx-auto mt-12 border-white text-white font-semibold border-2 py-4 px-8" onClick={() => {
-
-                if (id === 1234 && pwd === "password") {
-                    router.push('/appContent/academics')
-                    return
-                }
-                Notifications.NotificationQueue.push({
-                    action: ActionType.ERROR,
-                });
-
-                Notifications.updateNotificationQueue &&
-                    Notifications.updateNotificationQueue();
-            }}>LOGIN</button>
+            <button className="block w-fit mx-auto mt-12 border-white text-white font-semibold border-2 py-4 px-8" onClick={login}>LOGIN</button>
         </div>
     </div>
 }
